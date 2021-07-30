21e576d44d3e5b0d70310afaa821c86ac9660544
李志翀 <lizhichong@Retros-MPB.local>
2021-07-30 20:43:58 +0800

    refactor: update the router and API

--- routers/blog/router.go
+++ routers/blog/router.go
@@ -1,1 +1,20 @@
 package blog
+
+import "github.com/gin-gonic/gin"
+
+// InitBlogRouter initialize blog routing
+func InitBlogRouter() *gin.Engine {
+	router := gin.New()
+
+	blogApi := router.Group("api/blog")
+	{
+		blogApi.POST("article/listArticles")
+		blogApi.POST("article/getArticleDetails")
+		blogApi.POST("article/searchArticle")
+		blogApi.POST("article/listArticleTags")
+
+		blogApi.POST("friend/listFriends")
+	}
+
+	return router
+}
--- routers/router.go
+++ routers/router.go
@@ -1,1 +1,5 @@
 package routers
+
+type Server struct {
+
+}
