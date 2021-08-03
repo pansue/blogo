d4b390317d582e090b276c93ecdae8707699909b
feduang <893291581@qq.com>
2021-08-03 23:03:56 +0800

    update Tag module: add CRUD functions

--- models/tag.go
+++ models/tag.go
@@ -1,8 +1,70 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"blogo/global"
+	"gorm.io/gorm"
+)
 
 type Tag struct {
 	gorm.Model
 	Name string `gorm:"type:VARCHAR(255) NOT NULL"`
+}
+
+// AddTag Add a Tag
+func AddTag(name string) error {
+	tag := Tag{
+		Name: name,
+	}
+	if err := global.GORM.Create(&tag).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+// DeleteTag delete a tag
+func DeleteTag(id int) error {
+	if err := global.GORM.Where("id = ?", id).Delete(&Tag{}).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+// ExistTagByID determines whether a Tag exists based on the ID
+func ExistTagByID(id int) (bool, error) {
+	var tag Tag
+	err := global.GORM.Select("id").Where("id = ? AND deleted_on = ?", id, 0).First(&tag).Error
+	if err != nil && err != gorm.ErrRecordNotFound {
+		return false, err
+	}
+	if tag.ID > 0 {
+		return true, nil
+	}
+
+	return false, nil
+}
+
+// ExistTagByName checks if there is a tag with the same name
+func ExistTagByName(name string) (bool, error) {
+	var tag Tag
+	err := global.GORM.Select("id").Where("name = ? AND deleted_on = ? ", name, 0).First(&tag).Error
+	if err != nil && err != gorm.ErrRecordNotFound {
+		return false, err
+	}
+
+	if tag.ID > 0 {
+		return true, nil
+	}
+
+	return false, nil
+}
+
+// GetTags gets a list of tags
+func GetTags() ([]Tag, error) {
+	var tags []Tag
+	err := global.GORM.Select("*").Find(&tags).Error
+	if err != nil {
+		return tags, err
+	} else {
+		return nil, err
+	}
 }
