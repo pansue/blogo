f73340442c7fc99b42a79701a2f0652c7d1aea48
AlphaGHX <ChenYuanHangOL@outlook.com>
2021-08-07 23:16:05 +0800

    feat: Add LoginApi

--- api/system/sys_login.go
+++ api/system/sys_login.go
@@ -14,20 +14,20 @@ func (b *LoginApi) Login(c *response.GinContextE) {
 	var loginInfo request.LoginRequest
 	c.C.ShouldBindJSON(&loginInfo)
 	if loginInfo.Username == "" || loginInfo.Password == "" {
-		c.FailWithMessage(1, "账号或密码不能为空")
+		c.FailWithMessage(response.API_ERROR, "账号或密码不能为空")
 		return
 	}
 	_, err := account_service.CheckAuth(loginInfo.Username, loginInfo.Password)
 	if err != nil {
-		c.FailWithMessage(1, "账号或密码错误")
+		c.FailWithMessage(response.API_ERROR, "账号或密码错误")
 		return
 	}
 	accessToken, expireTime, err := utils.GetToken(123, loginInfo.Username)
 	if err != nil {
-		c.FailWithMessage(1, "获取Token失败")
+		c.FailWithMessage(response.API_ERROR, "获取Token失败")
 		return
 	}
-	c.OkWithDetailed(0, &response.LoginResponse{
+	c.OkWithDetailed(response.SUCCESS, &response.LoginResponse{
 		AccessToken: accessToken,
 		ExpireTime:  expireTime,
 		User:        loginInfo.Username},
